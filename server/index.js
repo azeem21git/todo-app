98b2bd81a4bd7614ac975f068c3a5539ddec446b
azeem21git <azeem21uni@gmail.com>
2026-01-19 19:38:17 +0530

    #

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose =require('mongoose')
+const cors = require('cors')
+const User= require('./model/User')
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/LogReg')
+.then(()=>{
+    console.log('DB connectetd')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+
+app.get('/api',(req,res)=>{
+    res.json(User)
+})
+
+
+
+
+
+app.listen(3000,()=>[
+    console.log("server is running on 3000")
+])
